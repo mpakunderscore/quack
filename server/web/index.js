5cafa42381be4b28ef35005ccf043a415962270c
mpakunderscore <mpakunderscore@gmail.com>
2017-04-26 03:55:27 +0300

    Web markers

--- server/web/index.js
+++ server/web/index.js
@@ -73,10 +73,27 @@ function placeUser(region) {
 
     } else {
 
+        let image = {
+            url: 'favicon.png',
+            // This marker is 20 pixels wide by 32 pixels high.
+            // size: new google.maps.Size(250, 250),
+            // The origin for this image is (0, 0).
+            origin: new google.maps.Point(0, 0),
+            // The anchor for this image is the base of the flagpole at (0, 32).
+            anchor: new google.maps.Point(35, 35),
+
+            scaledSize: new google.maps.Size(70, 70)
+        };
+
         user.marker = new google.maps.Marker({
+
             position: position,
-            map: map
+            map: map,
+            // label: 'Label',
+            icon: image
         });
+
+        // user.marker.setAnimation(google.maps.Animation.BOUNCE);
 
         markers.push(user);
     }
